ab1b57e9281cb09557a2c80058a3610ac19e8ff3
Edwin Paredes <paredes_844@hotmail.com>
2025-05-12 08:05:17 -0500

    Declaracion de arreglos

---
+++ Semana7/index.php
@@ -0,0 +1,17 @@
+<?php
+$animales = ['perro','gato','cuy'];
+print_r($animales);
+
+echo '<br>';
+
+$futbol = array('cristal','universitario','alianza');
+print_r($futbol);
+
+/*
+foreach($animales as $item)
+{
+    echo $item,'<br>';
+    
+}
+    */
+?>
