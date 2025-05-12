f168bb0e0d456ccc5f99cb4b2a1f1e32f4c0c068
Edwin Paredes <paredes_844@hotmail.com>
2025-05-12 08:30:22 -0500

    Arreglo asociativo

--- Semana7/index.php
+++ Semana7/index.php
@@ -7,6 +7,23 @@ echo '<br>';
 $futbol = array('cristal','universitario','alianza');
 print_r($futbol);
 
+echo '<br>';
+
+$asociativo = [1 => 'Edwin','0' => 'Paredes Vásquez','Edad' => 39];
+print_r($asociativo);
+
+echo '<br>';
+echo $animales[0];
+
+echo '<br>';
+echo $futbol[0];
+
+echo '<br>';
+echo $asociativo['Edad'];
+
+
+
+
 /*
 foreach($animales as $item)
 {
@@ -14,4 +31,7 @@ foreach($animales as $item)
     
 }
     */
+
+
+
 ?>
